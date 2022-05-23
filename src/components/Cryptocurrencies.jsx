b5feec1fa17bcc7c5d405699db616687f9fc40b4
Sam-kayode <marvelloussamkayode@gmail.com>
2022-05-23 14:57:06 +0100

    first  commit

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import millify from "millify";
+import { link } from "react-router-dom";
+import { Card, Col, Row, Input } from "antd";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const Crptocurrencies = () => {
+  return <div>Crptocurrencies</div>;
+};
+
+export default Crptocurrencies;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "4f17b4d4a3mshfe287a2ac704691p1fb98bjsn8f9cc0bbbd89",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({
+  url,
+  headers: cryptoApiHeaders,
+});
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
