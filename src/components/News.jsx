08d03fab950d42f1055be0e272b52a98db472efd
Sam-kayode <marvelloussamkayode@gmail.com>
2022-05-26 15:55:52 +0100

    crypto news service

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import momemnt from "moment";
 
 const News = () => {
-  return (
-    <div>News</div>
-  )
-}
+  return <div>News</div>;
+};
 
-export default News
+export default News;
