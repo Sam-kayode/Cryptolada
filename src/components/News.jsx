12ce21746dd61ce69795380be703d92cd3f3ceee
Sam-kayode <marvelloussamkayode@gmail.com>
2022-05-26 15:57:27 +0100

    import news api into news component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import momemnt from "moment";
-
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 const News = () => {
   return <div>News</div>;
 };
