f8f07d0c9c76367052eb09221b3887feb95fea4d
Sam-kayode <marvelloussamkayode@gmail.com>
2022-05-26 17:29:52 +0100

    add reducer for crypto news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import momemnt from "moment";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
-const News = () => {
+
+const { Text, Title } = Typography;
+const { option } = Select;
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "CryptoCurrency",
+    count: simplified ? 10 : 100,
+  });
+  console.log(cryptoNews);
   return <div>News</div>;
 };
 
