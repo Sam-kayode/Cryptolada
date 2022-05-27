52e5da73f8748509c94709a4ffa188e848931636
Sam-kayode <marvelloussamkayode@gmail.com>
2022-05-27 02:40:55 +0100

    add news page cards

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -5,13 +5,30 @@ import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
 const { Text, Title } = Typography;
 const { option } = Select;
+
 const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({
     newsCategory: "CryptoCurrency",
-    count: simplified ? 10 : 100,
+    count: simplified ? 6 : 12,
   });
-  console.log(cryptoNews);
-  return <div>News</div>;
+
+  if (!cryptoNews?.value) return "Loading.......";
+
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url}>
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>{news.name}</Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
