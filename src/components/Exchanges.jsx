61dc7e8be7c1a054d8ccbdd0bf1bd8c13f56597f
Sam-kayode <marvelloussamkayode@gmail.com>
2022-05-30 17:37:13 +0100

    add exchange table styling

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -6,12 +6,12 @@ import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 import { Cryptocurrencies, News } from "../components";
 
-const { Title,Text } = Typography;
+const { Title, Text } = Typography;
 const { Panel } = Collapse;
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetCryptosQuery();
-  const globalStats = data?.data?.stats;
+  const { data, isFetching } = useGetCryptosQuery(100);
+  const coins = data?.data?.coins;
   console.log(data);
 
   const onChange = (key) => {
@@ -21,19 +21,45 @@ const Exchanges = () => {
   return (
     <Col className="exchange-container">
       <Row>
-        <Col xs={6}><Text>Exchanges</Text></Col>
-        <Col xs={6}><Text>24h Trade Volume</Text></Col>
-        <Col xs={6}><Text>Market</Text></Col>
-        <Col xs={6}><Text>Change</Text></Col>
+        <Col xs={6}>
+          <Text>Exchanges</Text>
+        </Col>
+        <Col xs={6}>
+          <Text>24h Trade Volume</Text>
+        </Col>
+        <Col xs={6}>
+          <Text>Market</Text>
+        </Col>
+        <Col xs={6}>
+          <Text>Change</Text>
+        </Col>
       </Row>
 
       <Collapse defaultActiveKey={["1"]} onChange={onChange}>
-        <Panel showArrow={false} header={<>   <Col xs={6}>{1}. <img src="" alt="" /> {"Binance"}</Col>
-        <Col xs={6}>24h Trade volume</Col>
-        <Col xs={6}>Market</Col>
-        <Col xs={6}>Change</Col></>} key="1">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae itaque accusamus ipsam quasi quos? Corrupti provident in beatae. Quibusdam sequi quae ipsa voluptatem fugit nihil beatae a iusto doloremque ea!</p>
-        </Panel>
+        {coins.map((coin) => (
+          <Panel
+            showArrow={false}
+            header={
+              <>
+                {" "}
+                <Col xs={6}>
+                  {1}. <img src={coin.iconUrl} alt="" className="exchange-icon"/> {"Binance"}
+                </Col>
+                <Col xs={6}>24h Trade volume</Col>
+                <Col xs={6}>Market</Col>
+                <Col xs={6}>Change</Col>
+              </>
+            }
+            key="1"
+          >
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Recusandae itaque accusamus ipsam quasi quos? Corrupti provident
+              in beatae. Quibusdam sequi quae ipsa voluptatem fugit nihil beatae
+              a iusto doloremque ea!
+            </p>
+          </Panel>
+        ))}
       </Collapse>
     </Col>
   );
